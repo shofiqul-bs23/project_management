94aa3508fa69458fc674d22e2d96aadd0ce729f6
shofiqul-bs23 <shofiqul.islam@brainstation-23.com>
2022-05-10 11:35:08 +0600

    [EA- 1878] Old Requisition Button and related function is commented (may use it later for calling PO from 'requisition' form )

--- custom_addons/project_modified/models/project.py
+++ custom_addons/project_modified/models/project.py
@@ -39,27 +39,27 @@ class Project(models.Model):
         }
 
 
-
-    def requisitions(self):
-        data = [(0, 0, {"product_id": line.product_id.id, "name": line.product_id.name,
-                        "product_qty": (line.quantity - line.quantity_done), "price_unit": line.price,
-                        'product_uom': line.product_id.uom_id.id, 'estimation_line': line.id})
-                for line in self.estimation_line_ids
-                ]
-        for x in data:
-            if x[2]['product_qty'] == 0:
-                data.remove(x)
-
-        return {
-            'res_model': 'purchase.order',
-            'type': 'ir.actions.act_window',
-            'view_mode': 'form',
-            'view_id': self.env.ref("purchase.purchase_order_form").id,
-            # 'res_id': new_purchase_order.id,
-            'context': {'default_project_id': self.id,
-                        'default_order_line': data,
-                        }
-        }
+    ############################################# OLD REQUISITION Button Function ###################################
+    # def requisitions(self):
+    #     data = [(0, 0, {"product_id": line.product_id.id, "name": line.product_id.name,
+    #                     "product_qty": (line.quantity - line.quantity_done), "price_unit": line.price,
+    #                     'product_uom': line.product_id.uom_id.id, 'estimation_line': line.id})
+    #             for line in self.estimation_line_ids
+    #             ]
+    #     for x in data:
+    #         if x[2]['product_qty'] == 0:
+    #             data.remove(x)
+    #
+    #     return {
+    #         'res_model': 'purchase.order',
+    #         'type': 'ir.actions.act_window',
+    #         'view_mode': 'form',
+    #         'view_id': self.env.ref("purchase.purchase_order_form").id,
+    #         # 'res_id': new_purchase_order.id,
+    #         'context': {'default_project_id': self.id,
+    #                     'default_order_line': data,
+    #                     }
+    #     }
 
     def show_rfqs(self):
         self.ensure_one()
